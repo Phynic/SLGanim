2abc606a6795c6fd4d95a96214437b851530e0ee
Phynic <dream1994001@163.com>
2018-03-09 17:44:46 +0800

    camera

--- Assets/Scripts/Core/RTSCamera.cs
+++ Assets/Scripts/Core/RTSCamera.cs
@@ -29,16 +29,8 @@ public class RTSCamera : MonoBehaviour
         float currentX;
         float currentY;
         float currentZ;
-        currentX = transform.position.x;
-        currentY = transform.position.y;
-        currentZ = transform.position.z;
+        
 #if (UNITY_IOS || UNITY_ANDROID)
-        
-#elif (UNITY_STANDALONE || UNITY_EDITOR)
-
-        //float mouseWheel = Input.GetAxis("Mouse ScrollWheel");
-#endif
-        
         if (Input.touchCount == 2)
         {
             // Store both touches.
@@ -68,33 +60,56 @@ public class RTSCamera : MonoBehaviour
             currentX -= Input.GetTouch(0).deltaPosition.x / 80.0f * cameraMoveSpeed * Time.deltaTime;
             currentZ -= Input.GetTouch(0).deltaPosition.y / 80.0f * cameraMoveSpeed * Time.deltaTime;
         }
-        
+#elif (UNITY_STANDALONE || UNITY_EDITOR)
+
+        float mouseWheel = Input.GetAxis("Mouse ScrollWheel");
         if (!EventSystem.current.IsPointerOverGameObject())
         {
-            //if (Input.mousePosition.x <= 20)
-            //{
-            //    transform.Translate(Vector3.left * cameraMoveSpeed * Time.deltaTime);
-            //}
-            //if (Input.mousePosition.x >= (Screen.width - 20))
-            //{
-            //    transform.Translate(Vector3.right * cameraMoveSpeed * Time.deltaTime);
-            //}
-            //if (Input.mousePosition.y <= 20)
-            //{
-            //    transform.Translate(Vector3.back * cameraMoveSpeed * Time.deltaTime, Space.World);
-            //}
-            //if (Input.mousePosition.y >= (Screen.height - 20))
-            //{
-            //    transform.Translate(Vector3.forward * cameraMoveSpeed * Time.deltaTime, Space.World);
-            //}
             
-            //currentY -= mouseWheel * cameraScrollSpeed * Time.deltaTime;
-            //currentZ += mouseWheel * cameraScrollSpeed * Time.deltaTime;
-            currentX = Mathf.Clamp(currentX, minXPos, maxXPos);
-            currentY = Mathf.Clamp(currentY, minYPos, maxYPos);
-            currentZ = Mathf.Clamp(currentZ, minZPos - currentY, maxZPos - currentY);
+        }
+#endif
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            var hor = Vector3.Cross(Vector3.up, transform.right);
 
-            transform.position = new Vector3(currentX, currentY, currentZ);
+            
+
+            if (Input.mousePosition.x <= 20)
+            {
+                transform.Translate(-transform.right * cameraMoveSpeed * Time.deltaTime, Space.World);
+            }
+            if (Input.mousePosition.x >= (Screen.width - 20))
+            {
+                transform.Translate(transform.right * cameraMoveSpeed * Time.deltaTime, Space.World);
+            }
+            if (Input.mousePosition.y <= 20)
+            {
+                transform.Translate(hor * cameraMoveSpeed * Time.deltaTime, Space.World);
+            }
+            if (Input.mousePosition.y >= (Screen.height - 20))
+            {
+                transform.Translate(-hor * cameraMoveSpeed * Time.deltaTime, Space.World);
+            }
+
+            transform.Translate(transform.forward * mouseWheel * cameraScrollSpeed * Time.deltaTime, Space.World);
+            //currentX = transform.position.x;
+            //currentY = transform.position.y;
+            //currentZ = transform.position.z;
+
+
+
+            //currentX = transform.localPosition.x;
+            //currentY = transform.localPosition.y;
+            //currentZ = transform.localPosition.z;
+
+            ////currentPosition = transform.localPosition;
+
+
+            //currentX = Mathf.Clamp(currentX, minXPos, maxXPos);
+            //currentY = Mathf.Clamp(currentY, minYPos, maxYPos);
+            //currentZ = Mathf.Clamp(currentZ, minZPos - currentY, maxZPos - currentY);
+
+            //transform.position = new Vector3(currentX, currentY, currentZ);
         }
     }
 }
