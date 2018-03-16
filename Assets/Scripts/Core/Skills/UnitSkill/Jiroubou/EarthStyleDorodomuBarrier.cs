328dc131b1d23ec5e9dcf789587a346fa0c43aa7
Phynic <dream1994001@163.com>
2018-03-16 16:38:04 +0800

    土牢堂无初步

---
+++ Assets/Scripts/Core/Skills/UnitSkill/Jiroubou/EarthStyleDorodomuBarrier.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthStyleDorodomuBarrier : AttackSkill {
+
+    public override void SetLevel(int level)
+    {
+        
+    }
+
+}
