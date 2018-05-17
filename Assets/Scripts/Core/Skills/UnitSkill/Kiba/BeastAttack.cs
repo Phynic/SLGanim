01dcb566167c037d4c638d34b279c2519234a0a2
Phynic <dream1994001@163.com>
2018-05-17 17:50:28 +0800

    四脚术调整

--- Assets/Scripts/Core/Skills/UnitSkill/Kiba/BeastAttack.cs
+++ Assets/Scripts/Core/Skills/UnitSkill/Kiba/BeastAttack.cs
@@ -10,7 +10,7 @@ using DG.Tweening;
 /// </summary>
 
 public class BeastAttack : AttackSkill {
-	
+    FXManager fx;
     public override void SetLevel(int level)
     {
         damageFactor = damageFactor + (level - 1) * 5;
@@ -20,44 +20,18 @@ public class BeastAttack : AttackSkill {
 	protected override void InitSkill()
 	{
 		base.InitSkill ();
-		Camera.main.GetComponent<RTSCamera> ().FollowTarget (character.position);
-		//跳过去并停在被攻击对象的前一格
-		RoundManager.GetInstance ().Invoke (() => {
-			if (!animator.applyRootMotion) {
-				animator.applyRootMotion = true;//√上applyRootMotion，未测试是否存在bug，也暂时未发现使用该判定条件目前有何问题，只是单纯觉得可能不够严谨（心理作用）？
-			}
-			RoundManager.GetInstance ().Invoke (() => {
-				float time = 0.8f;
-				float distance = (focus - character.position).magnitude;
+        fx = FXManager.GetInstance();
+        var destination = focus - (focus - character.position).normalized;
+        Camera.main.GetComponent<RTSCamera> ().FollowTarget (character.position);
+        //跳过去并停在被攻击对象的前一格
 
-				//Debug.Log("距离是："+distance);
-				if (!animator.isMatchingTarget) {
-					//distance的数值会有波动，发现其实也不用Mathf.RoundToInt（）取整并比较1了（强迫症请无视），直接用>0.95f比较也可以用，暂未发现有不妥的bug出现
-					if (distance > 0.95f) {  
-						MatchPoint (focus - character.forward);
-					}
-				}
-				if (distance <= 0.45f) {
-					animator.InterruptMatchTarget (false);
-					character.Translate (focus - character.position, Space.World);
-					animator.applyRootMotion = false;
-				}
-				RoundManager.GetInstance ().Invoke (() => {
-					//Effect ();//我的施工现场，调整效果的时机的地方，暂时留着空位，无需要就删了吧
-					//GetHit ();
-					RoundManager.GetInstance ().Invoke (() => {
-						RoundManager.GetInstance ().Invoke (() => {
-							complete = true;
-						}, 0.5f);
-					}, hit * 0.2f);
-				}, time);
-			}, 0.1f);//我比一般的其他技能加快了这里，从1f—>0.1f，因为延时长了，animator.InterruptMatchTarget (false)会出问题
-		}, 0.1f);
-	}
+        var t = character.DOMove(destination, animator.GetCurrentAnimatorStateInfo(0).length * 0.22f);
+        t.SetEase(fx.curve1);
+        
+        RoundManager.GetInstance().Invoke(() => {
+            character.position = destination;
 
-	private void MatchPoint(Vector3 destination)
-	{
-		//MatchTargetWeightMask中的positionXYZWeight应该是localPosition，所以forward即可，因为人物会转向。
-		animator.MatchTarget(destination, character.rotation, AvatarTarget.Root, new MatchTargetWeightMask(Vector3.forward, 0f), 0.33f, 0.55f);
-	}
+        }, 2f);
+    }
+    
 }
