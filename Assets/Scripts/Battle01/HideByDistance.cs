160a7e21423f2f41023a0a31cfe9473422a2ed79
Phynic <dream1994001@163.com>
2018-06-05 12:09:15 +0800

    土牢堂无适配初步

---
+++ Assets/Scripts/Battle01/HideByDistance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideByDistance : MonoBehaviour {
+    float distance;
+    MeshRenderer meshRenderer;
+    Material m;
+    void Start()
+    {
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        m = meshRenderer.material;
+    }
+
+    void Update()
+    {
+        distance = Vector3.Distance(Camera.main.transform.position, transform.position);
+        var value = distance * 1f / 6f - 9f / 6f;
+        m.color = new Color(m.color.r, m.color.g, m.color.b, value);
+    }
+}
