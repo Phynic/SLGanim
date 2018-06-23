84b2ef7f2a683beb7932e3cfd5fcc471f76fffb0
Phynic <dream1994001@163.com>
2018-06-23 22:30:35 +0800

    1

--- Assets/Scripts/Core/Function/ChangeData.cs
+++ Assets/Scripts/Core/Function/ChangeData.cs
@@ -3,41 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeData {
-    
+    //valueMax用作外部显示，包含bonus部分在内。如需拆分显示，需要手动减去bonus值。
     public static bool ChangeValue(Transform character, string dataName, int value)
     {
         var list = character.GetComponent<CharacterStatus>().attributes;
+        SLG.Attribute data;
         if (list.Find(d => d.eName == dataName) == null)
             return false;
-        list.Find(d => d.eName == dataName).value = value;
-        //值大于零小于最大值
-        list.Find(d => d.eName == dataName).value = Mathf.Clamp(list.Find(d => d.eName == dataName).value, 0, list.Find(d => d.eName == dataName).valueMax);
+        else
+            data = list.Find(d => d.eName == dataName);
+        data.value = value;
+        //值大于零小于最大值，用于抹除值大于最大值的部分。
+        data.value = Mathf.Clamp(data.value, 0, data.valueMax);
         return true;
     }
 
+    //升级时调用改动。
     public static bool ChangeValueMax(Transform character, string dataName, int value)
     {
         var list = character.GetComponent<CharacterStatus>().attributes;
+        SLG.Attribute data;
         if (list.Find(d => d.eName == dataName) == null)
             return false;
-        list.Find(d => d.eName == dataName).valueMax = value;
+        else
+            data = list.Find(d => d.eName == dataName);
+        data.valueMax = value;
         //最大值大于等于零
-        list.Find(d => d.eName == dataName).valueMax = Mathf.Clamp(list.Find(d => d.eName == dataName).valueMax, 0, list.Find(d => d.eName == dataName).valueMax);
-        //值大于零小于最大值
-        list.Find(d => d.eName == dataName).value = Mathf.Clamp(list.Find(d => d.eName == dataName).value, 0, list.Find(d => d.eName == dataName).valueMax);
+        data.valueMax = Mathf.Clamp(data.valueMax, 0, data.valueMax);
+        //值大于零小于最大值，用于抹除值大于最大值的部分。
+        data.value = Mathf.Clamp(data.value, 0, data.valueMax);
         return true;
     }
 
+    //技能提升时改动。
     public static bool ChangeBonus(Transform character, string dataName, int value)
     {
         var list = character.GetComponent<CharacterStatus>().attributes;
+        SLG.Attribute data;
         if (list.Find(d => d.eName == dataName) == null)
             return false;
-        list.Find(d => d.eName == dataName).bonus = value;
+        else
+            data = list.Find(d => d.eName == dataName);
+        data.bonus = value;
         //奖励值大于等于零
-        list.Find(d => d.eName == dataName).bonus = Mathf.Clamp(list.Find(d => d.eName == dataName).bonus, 0, list.Find(d => d.eName == dataName).bonus);
-        //值大于零小于最大值
-        list.Find(d => d.eName == dataName).value = Mathf.Clamp(list.Find(d => d.eName == dataName).value, 0, list.Find(d => d.eName == dataName).valueMax);
+        data.bonus = Mathf.Clamp(data.bonus, 0, data.bonus);
+        //valueMax适应bonus改变。
+        data.valueMax = Mathf.Clamp(data.valueMax + data.bonus, 0, data.valueMax + data.bonus);
+        //值大于零小于最大值，用于抹除值大于最大值的部分。
+        data.value = Mathf.Clamp(data.value, 0, data.valueMax);
         return true;
     }
 }
