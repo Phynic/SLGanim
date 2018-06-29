81788d4f9ce3601de1ece961ac0720e95ca3d37c
Phynic <dream1994001@163.com>
2018-06-29 15:56:12 +0800

    八卦64掌初步

---
+++ Assets/Scripts/Core/Skills/UnitSkill/Neji/Hakke64.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hakke64 : AttackSkill {
+
+    public override void SetLevel(int level)
+    {
+        
+    }
+}
