bde0a5c66127e8330c41e6b3ab6b9f246e1eefb2
Phynic <dream1994001@163.com>
2018-09-06 17:47:56 +0800

    Gal场景初步

---
+++ Assets/Scripts/Gal/Controller_Gal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class Controller_Gal : MonoBehaviour {
+    public Image[] sprites;
+    public ScreenFader screenFader;
+
+    private void Start()
+    {
+        GameController.GetInstance().Invoke(() => {
+            ComicToon();
+        }, 2);
+    }
+
+    public void ComicToon()
+    {
+        foreach (var s in sprites)
+        {
+            s.transform.DOMove(new Vector3(Screen.width / 2, Screen.height / 2, 0), 1);
+            s.DOFade(1, 1);
+            
+        }
+    }
+
+
+    public void NextScene()
+    {
+        screenFader.FadeOut(() => {
+            SceneManager.LoadScene("Main");
+        }, true);
+    }
+}
