65b26a7a9283a485a4c0cf1ba36f75938e0f2237
Phynic <dream1994001@163.com>
2018-09-07 15:46:43 +0800

    1

--- Assets/Scripts/Gal/Controller_Gal.cs
+++ Assets/Scripts/Gal/Controller_Gal.cs
@@ -8,21 +8,29 @@ using DG.Tweening;
 public class Controller_Gal : MonoBehaviour {
     public Image[] sprites;
     public ScreenFader screenFader;
+    float comicStep = 3f;
 
     private void Start()
     {
         GameController.GetInstance().Invoke(() => {
-            ComicToon();
+            StartCoroutine(ComicToon());
         }, 2);
     }
 
-    public void ComicToon()
+    IEnumerator ComicToon()
     {
         foreach (var s in sprites)
         {
             s.transform.DOMove(new Vector3(Screen.width / 2, Screen.height / 2, 0), 1);
+            GameController.GetInstance().Invoke(() => {
+                s.transform.DOMove(new Vector3(0, Screen.height, 0), 1);
+            }, 2f);
+
             s.DOFade(1, 1);
-            
+            GameController.GetInstance().Invoke(() => {
+                s.DOFade(0, 1);
+            }, 2f);
+            yield return new WaitForSeconds(comicStep);
         }
     }
 
