12a06c55150bcc5e1c02c9cb4ddb2aa55e98bf37
Phynic <dream1994001@163.com>
2018-10-10 17:12:07 +0800

    Gal

---
+++ Assets/Scripts/Core/Function/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T> {
+    protected static T instance;
+
+    public static T GetInstance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        instance = this as T;
+    }
+}
