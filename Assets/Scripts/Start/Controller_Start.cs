281d950dade89649964fb99f4007234378762ec6
Phynic <dream1994001@163.com>
2018-11-04 14:55:48 +0800

    StartScene

--- Assets/Scripts/Start/Controller_Start.cs
+++ Assets/Scripts/Start/Controller_Start.cs
@@ -2,16 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Controller_Start : Singleton<Controller_Start>
 {
     public ScreenFader screenFader;
-    
+    public Image bcColor;
+    public Image bcTexture;
+    public Image art;
+    float loopTime = 15;
+    float doColorTime = 2;
+    int lastRandom = 0;
+    Vector3 originPosition;
     public void NextScene()
     {
         screenFader.FadeOut(() => {
             Global.GetInstance().NextScene("Gal");
         }, true);
+    }
+
+    private void Start()
+    {
+
+        originPosition = art.transform.position;
+        StartCoroutine(ArtLoop());
+    }
+
+    IEnumerator ArtLoop()
+    {
+        var random = Random.Range(1, 26);
+
+        if (lastRandom > 0)
+        {
+            while(random == lastRandom)
+            {
+                random = Random.Range(1, 26);
+            }
+        }
+
+        lastRandom = random;
+
+        string path = "Textures/Start/";
+
+        if (random < 10)
+            path += "0";
+        path += random.ToString();
+        var tex = Resources.Load(path) as Texture2D;
+        Resources.UnloadUnusedAssets();
+
+        art.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        art.SetNativeSize();
+        var color = ComputeMainColor(art.sprite.texture);
+
+        bcColor.DOColor(color, doColorTime).SetEase(Ease.Linear);
+        bcTexture.DOColor(new Color(color.r, color.g, color.b, bcTexture.color.a), doColorTime).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(doColorTime);
+
+        art.DOFade(1, 3).SetEase(Ease.Linear);
+        art.transform.DOMove(new Vector3(Screen.width / 2 - 150, Screen.height / 2, 0), loopTime).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(loopTime - 1);
+
+        art.DOFade(0, 1).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(1);
+
+        art.transform.position = originPosition;
+        
+        yield return StartCoroutine(ArtLoop());
+    }
+
+    Color ComputeMainColor(Texture2D img)
+    {
+        float r = 0;
+        float g = 0;
+        float b = 0;
+        
+        int width = img.width;
+        int height = img.height;
+        Color[] colors = new Color[width * height];
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                colors[i * height + j] = img.GetPixel(i, j);
+                r += colors[i * height + j].r;
+                g += colors[i * height + j].g;
+                b += colors[i * height + j].b;
+            }
+        }
+
+        r /= width * height;
+        g /= width * height;
+        b /= width * height;
+
+        // 计算明度
+        
+        return new Color(r, g, b);
     }
 }
 
