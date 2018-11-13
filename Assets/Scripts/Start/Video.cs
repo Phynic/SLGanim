97886a89938a8aa1f4f763a4f07b8724c0132e03
Phynic <dream1994001@163.com>
2018-11-13 22:27:51 +0800

    Logo
    mp4

---
+++ Assets/Scripts/Start/Video.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Video : MonoBehaviour {
+    VideoPlayer vp;
+	// Use this for initialization
+	private void Start ()
+    {
+        vp = GetComponent<VideoPlayer>();
+        vp.loopPointReached += FadeIn;
+    }
+	
+    private void FadeIn(VideoPlayer source)
+    {
+        transform.parent.GetComponent<ScreenFader>().FadeIn();
+        Destroy(gameObject);
+    }
+}
