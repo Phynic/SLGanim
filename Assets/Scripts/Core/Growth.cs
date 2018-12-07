c7a18cdd101e21912c7b4d3c95d3e645f65a4d8d
Phynic <dream1994001@163.com>
2018-12-07 17:20:33 +0800

    1

---
+++ Assets/Scripts/Core/Growth.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Growth {
+    public string roleEName;
+    public float hpG;
+    public float mpG;
+    public float atkG;
+    public float defG;
+    public float dexG;
+    public float expG;
+}
