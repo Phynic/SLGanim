edddaa2f8a2383d37b03f40b7a4a1b530b54cb45
Phynic <dream1994001@163.com>
2019-06-23 08:44:04 +0800

    Update Video.cs

--- Assets/Scripts/Start/Video.cs
+++ Assets/Scripts/Start/Video.cs
@@ -5,26 +5,23 @@ using UnityEngine;
 using UnityEngine.Video;
 
 public class Video : MonoBehaviour {
-    public Transform mainMenu;
     public bool playOnStart = true;
     VideoPlayer vp;
 	// Use this for initialization
 	private void Start ()
     {
         vp = GetComponent<VideoPlayer>();
-        vp.loopPointReached += FadeIn;
+        
         if (playOnStart)
         {
+            vp.loopPointReached += FadeIn;
             vp.transform.SetAsLastSibling();
             vp.Prepare();
             vp.prepareCompleted += s => { vp.Play(); };
         }
         else
         {
-            Destroy(vp.gameObject);
-            mainMenu.gameObject.SetActive(true);
-            var screenFader = GameObject.Find("Canvas").GetComponentInChildren<ScreenFader>();
-            screenFader.waitForEvent = false;
+            FadeIn(vp);
         }
     }
 	
