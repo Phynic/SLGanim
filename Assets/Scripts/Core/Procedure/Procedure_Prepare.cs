0f3ff3275233c2ef8acb540052e446dfe227e50a
Phynic <dream1994001@163.com>
2019-07-21 17:22:23 +0800

    1

--- Assets/Scripts/Core/Procedure/Procedure_Prepare.cs
+++ Assets/Scripts/Core/Procedure/Procedure_Prepare.cs
@@ -6,11 +6,11 @@ public class Procedure_Prepare : Procedure
 {
     protected override void OnProcedureEnter()
     {
-        
+        PrepareView.GetInstance().Open();
     }
 
     protected override void OnProcedureExit()
     {
-        
+        PrepareView.GetInstance().Close();
     }
 }
